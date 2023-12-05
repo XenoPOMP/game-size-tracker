69a882c6dcc96c57e2a3e0abccc33bbfc363e006
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-12-05 21:27:07 +0300

    create theme changing with useBodyClassnames

--- src/assets/providers/ThemeProvider/ThemeProvider.tsx
+++ src/assets/providers/ThemeProvider/ThemeProvider.tsx
@@ -1,14 +1,26 @@
 import cn from 'classnames';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import { ProviderProps } from '@providers/Provider.props';
 
 import useAppSettings from '@hooks/useAppSettings';
+import useBodyClassnames from '@hooks/useBodyClassnames';
 
 import styles from './ThemeProvider.module.scss';
 
 const ThemeProvider: FC<ProviderProps> = ({ children }) => {
 	const { theme } = useAppSettings();
+	const [registerClassnames, deleteClassnames] = useBodyClassnames();
+
+	const classGroupName = 'theme';
+
+	useEffect(() => {
+		deleteClassnames(classGroupName);
+
+		if (theme.get() === 'light') {
+			registerClassnames(classGroupName, ['light']);
+		}
+	}, [theme.get()]);
 
 	return <>{children}</>;
 };
