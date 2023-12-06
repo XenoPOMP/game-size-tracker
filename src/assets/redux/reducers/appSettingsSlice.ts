8311b768000e5712e30e6ebf3d0d0125f31087b7
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-12-06 22:35:14 +0300

    update version in redux

--- src/assets/redux/reducers/appSettingsSlice.ts
+++ src/assets/redux/reducers/appSettingsSlice.ts
@@ -10,7 +10,7 @@ export type AppSettings = {
 };
 
 const initialState: AppSettings = {
-	appVersion: '1.0.0-rc-1',
+	appVersion: '1.0.0-rc-2',
 	appName: 'React Vite Application',
 	language: 'en',
 	theme: 'dark',
