355717596f153c34b9516ce41887423a547cfd35
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-12-09 21:32:46 +0300

    change UiContainer and UiGrid

--- src/assets/components/ui/UiGrid/UiGrid.props.ts
+++ src/assets/components/ui/UiGrid/UiGrid.props.ts
@@ -1,3 +1,13 @@
-import { GridSettings } from '@utils/getUiSx';
+import { ComponentProps } from 'react';
 
-export interface UiGridProps extends GridSettings {}
+import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
+import { UiContainerProps } from '@/src/components/ui/UiContainer/UiContainer.props';
+
+export interface UiGridProps
+  extends Partial<{
+      columns: number;
+      rows: number;
+      gap: number | string;
+    }>,
+    UiContainerProps,
+    ComponentProps<typeof UiContainer> {}
