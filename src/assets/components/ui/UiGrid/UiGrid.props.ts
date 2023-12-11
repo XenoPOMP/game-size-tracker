3b07693352e42aac653b5b40e3607769c767718d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-12-12 00:44:13 +0300

    fix imports

--- src/assets/components/ui/UiGrid/UiGrid.props.ts
+++ src/assets/components/ui/UiGrid/UiGrid.props.ts
@@ -1,13 +1,13 @@
 import { ComponentProps } from 'react';
 
-import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
-import { UiContainerProps } from '@/src/components/ui/UiContainer/UiContainer.props';
+import UiContainer from '@ui/UiContainer/UiContainer';
+import { UiContainerProps } from '@ui/UiContainer/UiContainer.props';
 
 export interface UiGridProps
-  extends Partial<{
-      columns: number;
-      rows: number;
-      gap: number | string;
-    }>,
-    UiContainerProps,
-    ComponentProps<typeof UiContainer> {}
+	extends Partial<{
+			columns: number;
+			rows: number;
+			gap: number | string;
+		}>,
+		UiContainerProps,
+		ComponentProps<typeof UiContainer> {}
