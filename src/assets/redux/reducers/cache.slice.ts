68e6ce93fb88a71ca73e06ac092bad552ea743d5
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-12-11 20:43:45 +0300

    create cache for games

---
+++ src/assets/redux/reducers/cache.slice.ts
@@ -0,0 +1,46 @@
+import { Nullable } from '@xenopomp/advanced-types';
+
+import { createSlice } from '@reduxjs/toolkit';
+
+import type { ReduxAction } from '@redux/types';
+
+export type CacheState = {
+	officialGames: Record<
+		Exclude<GameInfo['category'], 'other'>,
+		Nullable<GameInfo[]>
+	>;
+
+	otherGames: Nullable<GameInfo[]>;
+};
+
+const initialState: CacheState = {
+	officialGames: {
+		steam: null,
+		egs: null,
+	},
+
+	otherGames: null,
+};
+
+const cacheSlice = createSlice({
+	name: 'cache',
+	initialState,
+	reducers: {
+		cacheSteamGames(state, { payload }: ReduxAction<GameInfo[]>) {
+			state.officialGames.steam = payload;
+		},
+
+		cacheEgsGames(state, { payload }: ReduxAction<GameInfo[]>) {
+			state.officialGames.egs = payload;
+		},
+
+		cacheCustomGames(state, { payload }: ReduxAction<GameInfo[]>) {
+			state.otherGames = payload;
+		},
+	},
+});
+
+export default cacheSlice.reducer;
+export const { cacheSteamGames, cacheEgsGames, cacheCustomGames } =
+	cacheSlice.actions;
+export const initialCacheState = cacheSlice.getInitialState();
